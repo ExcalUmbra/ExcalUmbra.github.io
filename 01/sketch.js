cd6f28c673e252d10b816f84ebb4bf558333cddb
ExcalUmbra <phantomgadget@shaw.ca>
2019-02-07 11:03:36 -0600

    Feburary 7th

--- 01/sketch.js
+++ 01/sketch.js
@@ -12,4 +12,9 @@ function setup() {
 
 function draw() {
   background(220);
+  fill(255, 204, 0);
+  ellipse(200, 200, 125);
+  fill(69, 420, 165);
+  ellipse(250, 250, 100);
+  rect(mouseX, 400, 75, 75);
 }
