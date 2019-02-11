93e955cd015f0fe6a1eb6dec69cc262655e8dd1d
ExcalUmbra <phantomgadget@shaw.ca>
2019-02-11 10:54:28 -0600

    Feb 11th/19

---
+++ 04/sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background(220, 0, 200);
+  
+  line(0, 50, 400, 300);
+  rectMode(CENTER);
+  rect(200, 150, 150, 150);
+  
+}
