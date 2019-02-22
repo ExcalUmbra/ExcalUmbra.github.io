8b13073169715236104112e4fd0095da97e194a7
ExcalUmbra <47330164+ExcalUmbra@users.noreply.github.com>
2019-02-21 20:12:03 -0600

    Primitive failure

--- Primitive/sketch.js
+++ Primitive/sketch.js
@@ -5,30 +5,43 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x = 0;
+let y = 0;
+let randomShapes = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  //background(...);
-  rect(10, 10, 150, 55);
-  fill(255);
-  rect(110, 20, 35, 35);
+  if (randomShapes === true) {
+    ellipse(random(0, windowHeight), (random(0, windowWidth), 55, 55));
+    if (key === "r") {
+      randomShapes = !randomShapes;
+    }
+  }
 }
 
 function keyPressed() {
   fill(random(0, 255), random(0, 255), random(0, 255));
   if (key === "w") {
-    ellipse(mouseX, mouseY, random(30, 20), random(0, 255));
+    rect(mouseX, mouseY, random(0, 255), random(0, 255));
   }
   if (key === "a") {
-    ellipse(mouseX, mouseY, random(10 ,255), random(10, 255));
+    rect(mouseX, mouseY, 60, 60, 40);
   }
   if (key === "s") {
-    ellipse(mouseX, mouseY, random(21 ,255), random(21, 255));
+    rect(mouseX, mouseY, 38, 31, 86, 20, 69, 63, 30, 76);
   }
   if (key === "d") {
-    ellipse(mouseX, mouseY, random(20 ,90), random(20, 90));
+    ellipse(mouseX, mouseY, random(20, 90), random(20, 90));
+  }
+  if (key === "b") {
+    background(255);
   }
 }
-function mouse
+function keyisPressed() {
+  if (key === "f") {
+    loop(x, y, random(0, 255), random(0, 255));
+  }
+}
