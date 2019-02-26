79b6255bb1419e222f5f7d2964fa98aace265fc6
ExcalUmbra <phantomgadget@shaw.ca>
2019-02-26 11:16:34 -0600

    Primitive Need work

--- Primitive/sketch.js
+++ Primitive/sketch.js
@@ -4,34 +4,47 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let x = 0;
-let y = 0;
+let anonymous = 150;
 let randomShapes = false;
+let x = 500;
+let y = 500;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(60);
 }
 
 function draw() {
   if (randomShapes === true) {
-    ellipse(random(0, windowHeight), (random(0, windowWidth), 55, 55));
-    if (key === "r") {
-      randomShapes = !randomShapes;
-    }
+    ellipse(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
+    triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
+    rect(x, y, random(30, 260), random(30, 260));
+    
+  }
+  ellipse(anonymous, 240, 240, 200, 200);
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  anonymous += 15;
+  if(anonymous > width + 100){
+    anonymous = 10;
   }
 }
 
 function keyPressed() {
+  if (key === "r") {
+    if (randomShapes === false) {
+      randomShapes = !randomShapes;
+      return false;
+    }
+  }
   fill(random(0, 255), random(0, 255), random(0, 255));
   if (key === "w") {
-    rect(mouseX, mouseY, random(0, 255), random(0, 255));
+    rect(mouseX, mouseY, random(30, 260), random(30, 260));
   }
   if (key === "a") {
-    rect(mouseX, mouseY, 60, 60, 40);
+    rect(mouseX, mouseY, random(0, 255), random(0, 255), random(0, 255));
   }
   if (key === "s") {
-    rect(mouseX, mouseY, 38, 31, 86, 20, 69, 63, 30, 76);
+    triangle(mouseX, mouseY, random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
   }
   if (key === "d") {
     ellipse(mouseX, mouseY, random(20, 90), random(20, 90));
--- varible states demo/sketch.js
+++ varible states demo/sketch.js
@@ -5,11 +5,52 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let buttonX, buttonY, buttonSize;
+let rectX, rectY, rectW, rectH;
+let rectShade = 0;
+let fadeIn = false;
+let buttonOver = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  buttonX = width / 2;
+  buttonY = height / 4;
+  buttonSize = 50;
+  rectX = width / 2;
+  rectY = height * 0.6;
+  rectW = width / 2;
+  rectH = height * 0.4;
+  noStroke();
+  ellipseMode(CENTER);
+  rectMode(CENTER);
 }
-
+function mouseDistance(x1, y1, x2, y2) {
+  let a = abs(x1 - x2);
+  let b = abs(y1 - y2);
+  let c = sqrt(sq(a) + sq(b));
+  return c;
+}
 function draw() {
-  background(220);
+  print(mouseDistance(mouseX, mouseY, buttonX, buttonY));
+  background(255);
+  //draw a circle
+  if (mouseDistance(mouseX, mouseY, buttonX, buttonY) < buttonSize / 2) {
+    fill(40, 20, 30);
+  }
+  else {
+    fill(75, 60, 65);
+    buttonOver = false;
+  }
+  ellipse(buttonX, buttonY, buttonSize, buttonSize);
+  //draw a rectangle
+  if (fadeIn = true); {
+    rectShade += 2;
+  }
+  fill(rectShade);
+  rect(rectX, rectY, rectW, rectH);
+}
+function mousePressed() {
+  if (buttonOver) {
+
+  }
 }
