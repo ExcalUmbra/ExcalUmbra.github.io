c240c861fa1c67e8357ea2506abe4aeb7c5ce9fd
ExcalUmbra <phantomgadget@shaw.ca>
2019-02-27 11:04:13 -0600

    Faded squares

--- Primitive/sketch.js
+++ Primitive/sketch.js
@@ -11,37 +11,35 @@ let y = 500;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(60);
+  frameRate(10);
 }
 
 function draw() {
   if (randomShapes === true) {
     ellipse(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
-    triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
-    rect(x, y, random(30, 260), random(30, 260));
-    
+    triangle(random(30, windowWidth), random(75, windowHeight), random(58, windowWidth), random(20, windowHeight), random(86, windowWidth), random(75, windowHeight));
+    rect(random(30, windowWidth), random(260, windowHeight), random(30, windowWidth), random(260, windowHeight));
+
   }
   ellipse(anonymous, 240, 240, 200, 200);
   fill(random(0, 255), random(0, 255), random(0, 255));
   anonymous += 15;
-  if(anonymous > width + 100){
+  if (anonymous > width + 100) {
     anonymous = 10;
   }
 }
 
 function keyPressed() {
   if (key === "r") {
-    if (randomShapes === false) {
-      randomShapes = !randomShapes;
-      return false;
-    }
+    randomShapes = !randomShapes;
+    return false;
   }
   fill(random(0, 255), random(0, 255), random(0, 255));
   if (key === "w") {
     rect(mouseX, mouseY, random(30, 260), random(30, 260));
   }
   if (key === "a") {
-    rect(mouseX, mouseY, random(0, 255), random(0, 255), random(0, 255));
+    rect(mouseX, mouseY, random(30, windowWidth), random(20, windowHeight), random(55, windowWidth));
   }
   if (key === "s") {
     triangle(mouseX, mouseY, random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
---
+++ State Variables Assignment Rollovers/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  rectMode(CENTER);
+  //fill()
+  rect(width/4, height/4, width/2, height/2);
+  rect(width/4, height - height/4, width/2, height/2);
+  rect(width - width/4, height/4, width/2, height/2);
+  rect(width - width/4, height - height/4, width/2, height/2);
+}
