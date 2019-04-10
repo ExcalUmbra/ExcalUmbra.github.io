f13edbbb15c381f860c159e042335788ecb57a82
ExcalUmbra <phantomgadget@shaw.ca>
2019-04-10 11:01:09 -0600

    Color demo complete

--- Color Solution Demo/sketch.js
+++ Color Solution Demo/sketch.js
@@ -5,39 +5,45 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Color Solutions Demo
 let rectWidth = 10;
 let rectHeight = 50;
-let colors = [];
-let counter = 0;
+let colors = ["#ECCE12", "#D3BB03", "#EF2149", "#900D1E", "#382C18" ];
+//let colors = [color(20,30,40),]
+
 function setup() {
+  noStroke();
   createCanvas(windowWidth, windowHeight);
   drawRowRGB(height * 0.2);
-  drawRowHSB(height /2);
+  drawRowHSB(height/2);
   drawRowCustom(height * 0.8);
 }
 
-function drawRowCustom(yPos) {
+function drawRowCustom(yPos){
   colorMode(RGB);
-  for (let x = 0; x < width; x += rectWidth) {
-    fill(colors[counter%5]);
-    rect(x, yPos, rectWidth, rectHeight);
-    counter++;
+  let counter = 0;
+  for (let x = 0; x < width; x+=rectWidth){
+    //fill(colors[counter%5]);
+    fill(colors[int(random(colors.length))]);
+    rect(x,yPos, rectWidth, rectHeight);
+    counter ++;
   }
 }
 
-function drawRowHSB(yPos) {
+function drawRowHSB(yPos){
   colorMode(HSB, 360);
-  for (let x = 0; x < width; x += rectWidth) {
-    fill(x / 10 % 360, 280, 330);
-    rect(x, yPos, rectWidth, rectHeight);
+  for (let x = 0; x < width; x+=rectWidth){
+    //fill (HUE,  SATURATION,  BRIGHTNESS)
+    fill( x/10 %360 , 280, 330 );
+    rect(x,yPos, rectWidth, rectHeight);
   }
 }
 
-function drawRowRGB(yPos) {
+function drawRowRGB(yPos){
   colorMode(RGB);
-  for (let x = 0; x < width; x += rectWidth) {
-    fill(random(255), random(255), random(255));
-    rect(x, yPos, rectWidth, rectHeight);
+  for (let x = 0; x < width; x+=rectWidth){
+    fill(random(255),random(255),random(255));
+    rect(x,yPos, rectWidth, rectHeight);
   }
 }
 
