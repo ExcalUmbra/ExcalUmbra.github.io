be2d3c101feafa27505ee1e52cf50a1891994139
ExcalUmbra <phantomgadget@shaw.ca>
2019-04-11 11:05:04 -0600

    Composition With Lines (Updated)

--- Composition With Lines/sketch.js
+++ Composition With Lines/sketch.js
@@ -4,19 +4,23 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-const squareSize = 15;
+let x = 0;
+let y = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
 }
 
-
 function drawComposition() {
+  x = random(width);
+  y = random(height);
+  if (dist(x,y,width/2, height/2)< 200){
+    ellipse(x,y,20 ,20);
+  }
 }
 
 
 function draw() {
-  background(220);
-
+  //background(220);
+  drawComposition();
 }
