b5fb211b1eb5d93c2c8e90692d99a2ff1865e92a
ExcalUmbra <phantomgadget@shaw.ca>
2019-04-11 10:14:24 -0600

    Saving & Resolution Demo

--- Sonic The Hedgehog/sketch.js
+++ Sonic The Hedgehog/sketch.js
@@ -23,14 +23,17 @@ function preload() {
   for (let a = 1; a < 6; a++) {
     pinImages.push(loadImage('assets/SonicAFK0' + a + '.png'));
   }
+  for (let s = 1; s < 6; s++) {
+    pinImages.push(loadImage('assets/SonicAFKBW0' + s + '.png'));
+  }
 }
 
 function draw() {
   frameRate(60);
   background(220);
-
-  image(pinImages[counter], speed, height / 2, 100, 200);
-
+  
+  image(pinImages[counter], speed * 5, height * 0.3, 200, 300);
+  //Left and Right Animation
   if (key === "ArrowLeft") {
     speed--;
     if (frameCount % 5 === 0) {
@@ -45,10 +48,17 @@ function draw() {
       if (counter > 16) counter = 9;
     }
   }
+  //Idle Animation
   if (key === " ") {
     speed;
     if (frameCount % 20 === 0)
       counter++;
     if (counter > 20 || counter < 16) counter = 16;
+  }
+  if (key === "v") {
+    speed;
+    if (frameCount % 20 === 0)
+      counter++;
+    if (counter > 25 || counter < 22) counter = 22;
   }
 }
