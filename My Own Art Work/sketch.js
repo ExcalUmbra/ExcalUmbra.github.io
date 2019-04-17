73b2f5a66e589d038eb01c516b195cfa0dad6011
ExcalUmbra <phantomgadget@shaw.ca>
2019-04-17 11:14:52 -0600

    My art Work in progress

--- My Own Art Work/sketch.js
+++ My Own Art Work/sketch.js
@@ -6,30 +6,29 @@
 // - describe what you did to take this project "above and beyond"
 const squareSize = 20;
 let black = 0;
-let squareX = 250;
-let squareY = 250;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER);
+  //rectMode(CENTER);
   noLoop();
 }
 
 function drawRectangles(offset) {
   for (let x = 0; x < width; x += squareSize) {
-    for (let y = 0; y < height; y += squareSize) {
-      noStroke();
-      setFill();
-      rect(squareX+offset, squareY+offset, squareSize, squareSize);
-      squareX -= squareSize;
-      squareY -= squareSize;
-    }
+    noStroke();
     setFill();
+    rect(x + offset, x, squareSize, squareSize);
   }
+  setFill();
 }
+
+
 
 function draw() {
   background(225);
-  drawRectangles(600.4);
+  for (let x = 0; x < 200; x += 50) {
+    drawRectangles(x);
+  }
+
 }
 function setFill() {
   if (black === 0) {
