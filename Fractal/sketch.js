00a184774b37fd40100bdbc4f9b1e7bd8da0c0db
ExcalUmbra <phantomgadget@shaw.ca>
2019-05-21 11:00:13 -0600

    Fractal

--- Fractal/sketch.js
+++ Fractal/sketch.js
@@ -4,8 +4,6 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-// 3D primites and Fractal
 let angle = 5;
 let angleSpeed = 1;
 
@@ -15,25 +13,26 @@ function setup() {
 }
 
 function draw() {
-  //angle = map(mouseX, 0, width, -120, 120);
+  fill(random(255),random(255),random(255));
+  angle = map(mouseX, 0, width, -30, 30);
   angle += angleSpeed;
-  if (angle < -40 || angle > 40) angleSpeed *= -1;
-  background(220);
+  if (angle < -20 || angle > 20) angleSpeed *= -1;
+  background(0);
   push();
-  rotateY(radians(frameCount));
+  rotateZ(radians(frameCount));
   for (let i = 0; i < 360; i += 45) {
     push();
-    rotateY(radians(i));
-    boxes(70);
+    rotateZ(radians(i));
+    boxes(50);
     pop();
   }
   pop();
 }
 
 function boxes(size) {
-  if (size > 10) {
+  if (size > 5) {
     rotateZ(radians(angle));
-    translate(size * 1.5, 0);
+    translate(size * 1.8, 0);
     box(size);
 
     boxes(size * 0.8);
